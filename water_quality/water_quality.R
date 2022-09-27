eec43efd810e6b0fba3f323b226b2a315b2ee2ea
beuri97 <beuri97@gmail.com>
2022-09-27 15:44:41 +1300

    modified water_quality.R

--- water_quality/water_quality.R
+++ water_quality/water_quality.R
@@ -3,21 +3,29 @@ library(tidyverse)
 
 # read the three datasets for use
 groundwater <- read_csv("groundwater_quality.csv")
-river_ecoli <- read_csv("river_ecoli.csv")
-river_nitrogen <- read_csv("river_nitrogen.csv")
+river_ecoli <- read_csv("new_river_ecoli.csv")
+river_nitrogen <- read_csv("new_river_nitrogen.csv")
 
-# takes the river_ecoli dataset then take the lat and long variables 
-# to get the full address. Then save it as new_riverecoli.
-new_riverecoli <- river_ecoli %>% 
-  reverse_geocode(lat = lat, long = long,
-                  method = "osm", full_results = TRUE)
 
-# takes the river_ecoli dataset then take the lat and long variables 
-# to get the full address. Then save it as new_rivernitrogen.
-new_rivernitrogen <- river_nitrogen %>% 
-  reverse_geocode(lat = lat, long = long,
-                  method = "osm", full_results = TRUE)
+# # takes the river_ecoli dataset then take the lat and long variables 
+# # to get the full address. Then save it as new_riverecoli.
+# new_riverecoli <- river_ecoli %>% 
+#   reverse_geocode(lat = lat, long = long,
+#                   method = "osm", full_results = TRUE)
+# 
+# # takes the river_ecoli dataset then take the lat and long variables 
+# # to get the full address. Then save it as new_rivernitrogen.
+# new_rivernitrogen <- river_nitrogen %>% 
+#   reverse_geocode(lat = lat, long = long,
+#                   method = "osm", full_results = TRUE)
+# 
+# # write the new dataset as CSVs for use
+# write_csv(new_riverecoli, "new_river_ecoli.csv")
+# write_csv(new_rivernitrogen, "new_river_nitrogen.csv")
 
-# write the new dataset as CSVs for use
-write_csv(new_riverecoli, "new_river_ecoli.csv")
-write_csv(new_rivernitrogen, "new_river_nitrogen.csv")
+groundwater_mod <- groundwater %>% 
+  select(region, measure, median, units, lower_confidence_level, upper_confidence_level) %>% 
+  filter(measure %in% c("Nitrate nitrogen", "E.coli")) %>% 
+  group_by(region, measure) %>% 
+  summarise(Median = sum(median), Units = units, Lower_CI = lower_confidence_level, Upper_CI = upper_confidence_level)
+groundwater_mod
