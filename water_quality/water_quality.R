9659a3004abd96c40470fbf2e6e42a3e1121531e
alz-droid <root@DESKTOP-A7FK83I.localdomain>
2022-10-01 20:46:05 +1300

    added new comments

--- water_quality/water_quality.R
+++ water_quality/water_quality.R
@@ -17,13 +17,18 @@ library(visdat)
 
 library(lubridate)
 
-# read the three datasets for use
+# Read the groundwq_2004-2020.xlsx and store it as groundwq for analysis.
 groundwq <- "groundwq_2004-2020.xlsx" %>% 
   read_excel()
 
+# Gives an overview of groundwq such as columns, data types, the possible values, number of rows and columns.
 groundwq %>% 
   glimpse()
 
+# The following lines of code show the entirety of the summary statistics of the whole groundwq.
+# The skim() provides a detailed overview of the dataframe.
+# The select() choose the columns specified.
+# the kable() creates and presents the overview of the dataframe in a table format.
 groundwq %>% 
   skim() %>% 
   select(1:7) %>% 
@@ -39,9 +44,13 @@ groundwq %>%
   select(15:21) %>% 
   kable()
 
+# Reads the entirety of groundwq and creates a plot to check if it contains missing data (NA).
 groundwq %>% 
   vis_miss(warn_large_data = FALSE)
 
+# Takes the groundwq modify CensoredValue and create a new variable called Year, select the relevant columns 
+# and rows, group the rows by Region, Indicator, and Year then create an new data frame containing the sum of
+# the CensoredValue rounded off to 2.
 new_groundwq <- groundwq %>% 
   mutate(CensoredValue = ifelse(is.na(CensoredValue), NA_integer_, CensoredValue),
          Year = year(Date)) %>% 
@@ -50,15 +59,18 @@ new_groundwq <- groundwq %>%
   group_by(Region, Indicator, Year) %>% 
   summarise(Value = sum(round(CensoredValue,2)))
 
-groundwq_wide <- ground_df %>% 
+# Takes the new_groundwq then convert it to wide format.
+groundwq_wide <- new_groundwq %>% 
   spread(key = Indicator,
          value = Value)
 groundwq_wide
 
 
 river_ecoli <- read_csv("new_river_ecoli.csv")
-river_nitrogen <- read_csv("new_river_nitrogen.csv")
 
+# Read the new_river_nitrogen.csv and store it as river_nitrogen for analysis.
+river_nitrogen <- "new_river_nitrogen.csv" %>% 
+  read_csv()
 
 # # takes the river_ecoli dataset then take the lat and long variables 
 # # to get the full address. Then save it as new_riverecoli.
